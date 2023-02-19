5b124222b28dbf054cff103d60fe059d44ddbf41
Luoxi Pan <l.panpax@gmail.com>
2023-02-19 21:52:25 +1100

    Style and load Card component from asset object

--- src/App.js
+++ src/App.js
@@ -3,15 +3,52 @@ import Card from './components/Card';
 import Header from './components/Header';
 import './App.css';
 
+// Jupiter moons
+import ioImg from './images/io.jpg';
+import europaImg from './images/europa.jpeg';
+import callistoImg from './images/callisto.jpeg';
+// Saturn Moons
+import enceladusImg from './images/enceladus.jpeg';
+import titanImg from './images/titan.jpeg';
+// Neptune moons
+import rosalindImg from './images/rosalind.jpeg';
+
+const assets = {
+    io: {
+        moonImg: ioImg,
+        moon3D: '../images/io.usdz',
+    },
+    europa: {
+        moonImg: europaImg,
+        moon3D: '../images/europa.usdz',
+    },
+    enceladus: {
+        moonImg: enceladusImg,
+        moon3D: null,
+    },
+    callisto: {
+        moonImg: callistoImg,
+        moon3D: null,
+    },
+    titan: {
+        moonImg: titanImg,
+        moon3D: null,
+    },
+    rosalind: {
+        moonImg: rosalindImg,
+        moon3D: null,
+    },
+};
+
 function App() {
-    const numCards = 12;
+    const appTitle = 'The Many Moons Memory Game';
 
     return (
         <div className="app-container">
-            <Header title={'Something Something Memory Game'} />
+            <Header title={appTitle} />
             <div className="card-grid">
-                {Array.from(Array(numCards).keys()).map((i) => (
-                    <Card key={i} />
+                {Object.entries(assets).map(([key, value]) => (
+                    <Card key={key} cardName={key} imgUrl={value.moonImg} />
                 ))}
             </div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../styles/Card.css';
 
-export default function Card() {
+export default function Card(props) {
+    const { cardName, imgUrl } = props;
+
     return (
         <div className="card">
-            <div className="card-title">Card 1</div>
-            <div className="card-image">Image</div>
+            <div className="card-title">{cardName[0].toUpperCase() + cardName.substring(1)}</div>
+
+            <img className="card-img" src={imgUrl} alt="" />
         </div>
     );
 }
