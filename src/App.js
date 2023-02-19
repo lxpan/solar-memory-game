a3f396e5849dd71873873bbe5608575ad59fd471
Luoxi Pan <l.panpax@gmail.com>
2023-02-19 14:46:28 +1100

    Create bare bone components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from './components/Card';
+import ScoreBoard from './components/ScoreBoard';
 import './App.css';
 
 function App() {
-    return <div>Memory Game</div>;
+    return (
+        <div>
+            <h2>Something Something Memory Game</h2>
+            <ScoreBoard />
+            <Card />
+        </div>
+    );
 }
 
 export default App;
