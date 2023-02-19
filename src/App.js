938429fe08c21b0cd2cef53861d317b31245bbb4
Luoxi Pan <l.panpax@gmail.com>
2023-02-20 01:48:04 +1100

    Add 3D planet models to Cards, replacing 2D images

--- src/App.js
+++ src/App.js
@@ -3,42 +3,18 @@ import Card from './components/Card';
 import Header from './components/Header';
 import './App.css';
 
-// Jupiter moons
-import ioImg from './assets/images/io.jpg';
-import europaImg from './assets/images/europa.jpeg';
-import callistoImg from './assets/images/callisto.jpeg';
-// Saturn Moons
-import enceladusImg from './assets/images/enceladus.jpeg';
-import titanImg from './assets/images/titan.jpeg';
-// Neptune moons
-import rosalindImg from './assets/images/rosalind.jpeg';
-
-const assets = {
-    io: {
-        moonImg: ioImg,
-        moon3D: null,
-    },
-    europa: {
-        moonImg: europaImg,
-        moon3D: null,
-    },
-    enceladus: {
-        moonImg: enceladusImg,
-        moon3D: null,
-    },
-    callisto: {
-        moonImg: callistoImg,
-        moon3D: null,
-    },
-    titan: {
-        moonImg: titanImg,
-        moon3D: null,
-    },
-    rosalind: {
-        moonImg: rosalindImg,
-        moon3D: null,
-    },
-};
+const PLANETS = [
+    'venus',
+    'mercury',
+    'earth',
+    'moon',
+    'mars',
+    'jupiter',
+    'saturn',
+    'uranus',
+    'neptune',
+    'pluto',
+];
 
 function App() {
     const appTitle = 'The Many Moons Memory Game';
@@ -47,8 +23,8 @@ function App() {
         <div className="app-container">
             <Header title={appTitle} />
             <div className="card-grid">
-                {Object.entries(assets).map(([key, value]) => (
-                    <Card key={key} cardName={key} imgUrl={value.moonImg} />
+                {PLANETS.map((planet) => (
+                    <Card key={planet} cardName={planet} />
                 ))}
             </div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,175 @@
-import React from 'react';
+import React, { Suspense, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stage, useGLTF } from '@react-three/drei';
 import '../styles/Card.css';
 
+// planet model imports
+import mercuryModel from '../assets/models/mercury.glb';
+import venusModel from '../assets/models/venus.glb';
+import earthModel from '../assets/models/earth.glb';
+import moonModel from '../assets/models/moon.glb';
+import marsModel from '../assets/models/mars.glb';
+import jupiterModel from '../assets/models/jupiter.glb';
+import saturnModel from '../assets/models/saturn.glb';
+import uranusModel from '../assets/models/uranus.glb';
+import neptuneModel from '../assets/models/neptune.glb';
+import plutoModel from '../assets/models/pluto.glb';
+
+const modelMap = {
+    mercury: {
+        model: mercuryModel,
+    },
+    venus: {
+        model: venusModel,
+    },
+    earth: {
+        model: earthModel,
+    },
+    moon: {
+        model: moonModel,
+    },
+    mars: {
+        model: marsModel,
+    },
+    jupiter: {
+        model: jupiterModel,
+    },
+    saturn: {
+        model: saturnModel,
+    },
+    uranus: {
+        model: uranusModel,
+    },
+    neptune: {
+        model: neptuneModel,
+    },
+    pluto: {
+        model: plutoModel,
+    },
+};
+
+Object.values(modelMap).forEach((planet) => {
+    useGLTF.preload(planet.model);
+});
+
+function Model(props) {
+    const { planetName } = props;
+    const { nodes, materials } = useGLTF(modelMap[planetName].model);
+
+    function getMesh(planetName) {
+        switch (planetName) {
+            case 'mercury':
+                return (
+                    <mesh
+                        geometry={nodes.Cube008.geometry}
+                        material={materials['Default OBJ.005']}
+                    />
+                );
+
+            case 'venus':
+                return (
+                    <mesh
+                        geometry={nodes.cylindrically_mapped_sphereMesh.geometry}
+                        material={materials['Default OBJ']}
+                    />
+                );
+
+            case 'earth':
+                return (
+                    <mesh geometry={nodes.Cube001.geometry} material={materials['Default OBJ']} />
+                );
+
+            case 'moon':
+                return (
+                    <mesh
+                        geometry={nodes.Cube008.geometry}
+                        material={materials['Default OBJ.005']}
+                    />
+                );
+
+            case 'mars':
+                return (
+                    <mesh
+                        geometry={nodes.Cube008.geometry}
+                        material={materials['Default OBJ.005']}
+                    />
+                );
+
+            case 'jupiter':
+                return <mesh geometry={nodes.cubemap.geometry} material={materials.None} />;
+
+            case 'saturn':
+                return (
+                    <>
+                        <mesh geometry={nodes.RingsTop.geometry} material={materials.SaturnRings} />
+                        <mesh
+                            geometry={nodes.RingsBottom.geometry}
+                            material={materials.SaturnRings}
+                        />
+                        <mesh geometry={nodes.Saturn001.geometry} material={materials.None} />
+                    </>
+                );
+
+            case 'uranus':
+                return (
+                    <mesh
+                        geometry={nodes.Uranus.geometry}
+                        material={materials['Default OBJ.001']}
+                        rotation={[Math.PI / 2, 0, 0]}
+                    />
+                );
+
+            case 'neptune':
+                return (
+                    <mesh
+                        geometry={nodes.Neptune.geometry}
+                        material={materials['Default OBJ.001']}
+                        rotation={[Math.PI / 2, 0, 0]}
+                    />
+                );
+
+            case 'pluto':
+                return (
+                    <mesh
+                        geometry={nodes.cylindrically_mapped_sphere.geometry}
+                        material={materials['Default OBJ.001']}
+                    />
+                );
+
+            default:
+                break;
+        }
+    }
+
+    return (
+        <group {...props} dispose={null}>
+            {getMesh(planetName)}
+        </group>
+    );
+}
+
 export default function Card(props) {
-    const { cardName, imgUrl } = props;
+    const { cardName } = props;
+    const ref = useRef();
 
     return (
         <div className="card">
             <div className="card-title">{cardName[0].toUpperCase() + cardName.substring(1)}</div>
 
-            <img className="card-img" src={imgUrl} alt="" />
+            <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
+                <Suspense fallback={null}>
+                    <Stage
+                        controls={ref}
+                        preset="rembrandt"
+                        intensity={1}
+                        shadows={false}
+                        environment="city"
+                    >
+                        <Model planetName={cardName} />
+                    </Stage>
+                </Suspense>
+                <OrbitControls ref={ref} />
+            </Canvas>
         </div>
     );
 }
